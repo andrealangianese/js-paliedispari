ef794c9f670e015e06955ebe8499c3cf1749873d
andrealangianese <andre.langia.01@gmail.com>
2025-12-05 20:27:18 +0100

    feat: fine secondo esercizio

--- paridispari/script1.js
+++ paridispari/script1.js
@@ -15,3 +15,55 @@
 // Ho bisogno di parametri?
 // Devo restituire un valore?
 // Se sì, di che tipo?
+
+
+let userChoice = prompt("scegli un valore tra pari e dispari");
+let userNumber = parseInt(prompt("inserisci un numero tra 1 e 5"));
+
+//generiamo tramite funzione un numero tra 1 e 5 per il computer
+
+function myNumber() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+// inserisco una variabile per assegnare il valore della funzione, altrimenti non posso utilizzare myNumber nella somma che ho bisogno dopo, perchè mi sballa tutto
+
+let pcNumber = myNumber();
+
+// facciamo la somma tra i 2 numeri
+
+let somma = pcNumber + userNumber;
+
+
+
+//faccio un console log per vedere i numeri
+
+console.log(somma, " questa sarebbe la somma");
+console.log(userNumber, "questa sarebbe la mia scelta di numero");
+console.log(pcNumber,", questa la scelta randomica del sistema");
+console.log(userChoice, ", questa sarebbe la mia scelta tra pari e dispari");
+
+
+
+
+
+//tramite funzione indichiamo se la somma è pari o no
+
+function isPari(somma) {
+    if (somma % 2 === 0) {
+        return "pari"
+    }
+    else {
+        return "dispari"
+    }
+}
+
+// dichiarazione del vincitore
+
+if (userChoice === isPari(somma)) {
+    {
+        console.log("hai vinto !!!!!")
+    }
+} else {
+    console.log("ritenta sarai più fortunato!!")
+}
